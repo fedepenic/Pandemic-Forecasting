a4373de7314f7685baefef9089ac9fd133784e63
fedepenic <fedepenic@hotmail.com>
2023-07-21 09:28:05 -0300

    Refactor regex method

--- main.go
+++ main.go
@@ -11,43 +11,72 @@ import (
 )
 
 func main() {
-    url := "https://www.worldometers.info/coronavirus/country/south-africa/" // Replace this with the URL of the webpage you want to parse
+	url := "https://www.worldometers.info/coronavirus/country/south-africa/" // Replace this with the URL of the webpage you want to parse
 
-    // Fetch the webpage
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Fatalf("Error fetching the webpage: %v", err)
-    }
-    defer resp.Body.Close()
+	// Fetch the webpage
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error fetching the webpage: %v", err)
+	}
+	defer resp.Body.Close()
 
-    // Parse the HTML content
-    doc, err := html.Parse(resp.Body)
-    if err != nil {
-        log.Fatalf("Error parsing HTML: %v", err)
-    }
+	// Parse the HTML content
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		log.Fatalf("Error parsing HTML: %v", err)
+	}
 
-    // Get the content of <script type="text/javascript"> tags containing "graph-cases-daily"
-    content := getScriptContents(doc, "graph-cases-daily")
+	// Get the content of <script type="text/javascript"> tags containing "graph-cases-daily"
+	content := getScriptContents(doc, "graph-cases-daily")
 
-    // re := regexp.MustCompile(`categories:\s*\[(.*?)\]`)
+	// Extract content using the specified regular expression
+	extractedContent := extractContent(content, `data:\s*\[(.*?)\]`)
+    extractedContentDates := extractContent(content, `categories:\s*\[(.*?)\]`)
 
-    // match := re.FindStringSubmatch(content)
+	fmt.Println(extractedContent)
+    fmt.Println(extractedContentDates)
+}
 
-	// var finalContent string
+// Recursive function to get the contents of <script type="text/javascript"> tags
+// if they contain the specified string and return it as a string.
+func getScriptContents(n *html.Node, targetString string) string {
+	if n.Type == html.ElementNode && n.Data == "script" {
+		for _, attr := range n.Attr {
+			if attr.Key == "type" && attr.Val == "text/javascript" {
+				content := getTextContent(n)
+				if strings.Contains(content, targetString) {
+					// Return the content of the script tag
+					return content
+				}
+			}
+		}
+	}
+	var scriptContent string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		scriptContent = getScriptContents(c, targetString)
+		if scriptContent != "" {
+			return scriptContent
+		}
+	}
+	return ""
+}
 
-	// if len(match) > 1 {
-	// 	// Extracted content is in match[1]
-	// 	categoriesContent := match[1]
-	// 	finalContent = "[" + categoriesContent + "]"
-	// } else {
-	// 	finalContent = "Categories not found in the input."
-	// }
+// Helper function to get the text content of an HTML node and its descendants
+func getTextContent(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	var textContent string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		textContent += getTextContent(c)
+	}
+	return textContent
+}
 
-	// fmt.Println(finalContent)
-
-    re := regexp.MustCompile(`data:\s*\[(.*?)\]`)
-
-    match := re.FindStringSubmatch(content)
+// Function to extract content using a regular expression
+func extractContent(content, regex string) string {
+	re := regexp.MustCompile(regex)
+	match := re.FindStringSubmatch(content)
 
 	var finalContent string
 
@@ -56,46 +85,8 @@ func main() {
 		categoriesContent := match[1]
 		finalContent = "[" + categoriesContent + "]"
 	} else {
-		finalContent = "Categories not found in the input."
+		finalContent = "Content not found in the input."
 	}
 
-	fmt.Println(finalContent)
-
-    // fmt.Println(content)
-}
-
-// Recursive function to get the contents of <script type="text/javascript"> tags
-// if they contain the specified string and return it as a string.
-func getScriptContents(n *html.Node, targetString string) string {
-    if n.Type == html.ElementNode && n.Data == "script" {
-        for _, attr := range n.Attr {
-            if attr.Key == "type" && attr.Val == "text/javascript" {
-                content := getTextContent(n)
-                if strings.Contains(content, targetString) {
-                    // Return the content of the script tag
-                    return content
-                }
-            }
-        }
-    }
-    var scriptContent string
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        scriptContent = getScriptContents(c, targetString)
-        if scriptContent != "" {
-            return scriptContent
-        }
-    }
-    return ""
-}
-
-// Helper function to get the text content of an HTML node and its descendants
-func getTextContent(n *html.Node) string {
-    if n.Type == html.TextNode {
-        return n.Data
-    }
-    var textContent string
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        textContent += getTextContent(c)
-    }
-    return textContent
+	return finalContent
 }
