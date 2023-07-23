b3bac18fd40071e324c39ac129f7802b05901446
fedepenic <fedepenic@hotmail.com>
2023-07-22 22:01:23 -0300

    Remove comments

--- main.go
+++ main.go
@@ -15,25 +15,21 @@ import (
 )
 
 func main() {
-	url := "https://www.worldometers.info/coronavirus/country/south-africa/" // Replace this with the URL of the webpage you want to parse
+	url := "https://www.worldometers.info/coronavirus/country/south-africa/" 
 
-	// Fetch the webpage
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("Error fetching the webpage: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// Parse the HTML content
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
 		log.Fatalf("Error parsing HTML: %v", err)
 	}
 
-	// Get the content of <script type="text/javascript"> tags containing "graph-cases-daily"
 	content := getScriptContents(doc, "graph-cases-daily")
 
-	// Extract content using the specified regular expression
 	extractedContent := extractContent(content, `data:\s*\[(.*?)\]`)
 	extractedContentDates := extractContent(content, `categories:\s*\[(.*?)\]`)
 
@@ -44,27 +40,22 @@ func main() {
 		return
 	}
 
-	// Verify that the length of arr and arrDates match
 	if len(arr) != len(arrDates) {
 		fmt.Println("Error: Lengths of arrays do not match.")
 		return
 	}
 
-	// Create and open the CSV file
 	file, err := os.Create("data.csv")
 	if err != nil {
 		log.Fatalf("Error creating CSV file: %v", err)
 	}
 	defer file.Close()
 
-	// Create a CSV writer
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	// Write the CSV header
 	writer.Write([]string{"Date", "Value"})
 
-	// Write data rows to CSV
 	for i := 0; i < len(arr); i++ {
 		writer.Write([]string{arrDates[i], strconv.Itoa(arr[i])})
 	}
@@ -83,14 +74,11 @@ func convertStringDatesToArray(str string) ([]string, error) {
 }
 
 func convertStringToArray(str string) []int {
-	// Step 1: Parse the string into a slice of strings
-	str = strings.Trim(str, "[]") // Remove square brackets from the string
+	str = strings.Trim(str, "[]") 
 	strArr := strings.Split(str, ",")
 
-	// Step 2: Convert the slice of strings into a slice of integers
 	var intArr []int
 	for _, s := range strArr {
-		// Replace "null" with "0" and parse the string to an integer
 		if s == "null" {
 			intArr = append(intArr, 0)
 		} else {
@@ -103,15 +91,12 @@ func convertStringToArray(str string) []int {
 	return intArr
 }
 
-// Recursive function to get the contents of <script type="text/javascript"> tags
-// if they contain the specified string and return it as a string.
 func getScriptContents(n *html.Node, targetString string) string {
 	if n.Type == html.ElementNode && n.Data == "script" {
 		for _, attr := range n.Attr {
 			if attr.Key == "type" && attr.Val == "text/javascript" {
 				content := getTextContent(n)
 				if strings.Contains(content, targetString) {
-					// Return the content of the script tag
 					return content
 				}
 			}
@@ -127,7 +112,6 @@ func getScriptContents(n *html.Node, targetString string) string {
 	return ""
 }
 
-// Helper function to get the text content of an HTML node and its descendants
 func getTextContent(n *html.Node) string {
 	if n.Type == html.TextNode {
 		return n.Data
@@ -139,7 +123,6 @@ func getTextContent(n *html.Node) string {
 	return textContent
 }
 
-// Function to extract content using a regular expression
 func extractContent(content, regex string) string {
 	re := regexp.MustCompile(regex)
 	match := re.FindStringSubmatch(content)
@@ -147,7 +130,6 @@ func extractContent(content, regex string) string {
 	var finalContent string
 
 	if len(match) > 1 {
-		// Extracted content is in match[1]
 		categoriesContent := match[1]
 		finalContent = "[" + categoriesContent + "]"
 	} else {
--- prediction.py
+++ prediction.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 def read_csv(file_path):
-    date_parser = lambda x: pd.to_datetime(x, format='%b %d, %Y')  # Change the date format here
+    date_parser = lambda x: pd.to_datetime(x, format='%b %d, %Y')  
     return pd.read_csv(file_path, parse_dates=['Date'], date_parser=date_parser, index_col='Date')
 
 def fit_arima(data, order):
@@ -14,43 +14,32 @@ def forecast_arima(model, steps):
     return model.forecast(steps)
 
 def main():
-    # Replace 'data.csv' with the path to your CSV file
     file_path = 'data.csv'
     df = read_csv(file_path)
 
-    # Assuming the CSV file has columns named 'Date' and 'Value'
     value_col = 'Value'
 
-    # Create a time series from the 'Value' column
     time_series = pd.Series(df[value_col].values, index=df.index)
 
-    # ARIMA model order (p, d, q)
     order = (1, 1, 1)
 
-    # Fit the ARIMA model
     model = fit_arima(time_series, order)
 
-    # Number of steps ahead to forecast
     steps = 7
 
-    # Forecast the next 'steps' values
     forecasted_values = forecast_arima(model, steps)
 
-    # Generate dates for the forecasted values
     last_date = df.index[-1]
     forecasted_dates = pd.date_range(start=last_date + pd.DateOffset(days=1), periods=steps, freq='D')
 
-    # Combine the forecasted dates and values into a dataframe
     forecast_df = pd.DataFrame({value_col: forecasted_values}, index=forecasted_dates)
 
     print("Forecasted values:")
     print(forecast_df)
 
-    # Save the forecasted values to a CSV file
     output_csv_path = 'forecasted_values.csv'
     forecast_df.to_csv(output_csv_path, index_label='date', header=['new_cases'])
 
-    # Plot the forecasted values
     plt.figure(figsize=(10, 5))
     plt.plot(df, label='Original Data')
     plt.plot(forecast_df, label='Forecasted Data', linestyle='dashed', marker='o')
@@ -58,11 +47,9 @@ def main():
     plt.ylabel('Value')
     plt.legend()
 
-    # Save the plot as a PNG file
     output_plot_path = 'forecast_plot.png'
     plt.savefig(output_plot_path)
 
-    # Show the plot
     plt.show()
 
 if __name__ == "__main__":
